96df430fb7c728e6e98861a7c8742c5dfa14c30a
Byron Angulo <baangulop1@utn.edu.ec>
2026-01-18 09:10:55 -0500

    cambio en el nombre

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import flask
 app = flask(__name__)
 @app.route('/')
 def hola():
-    return "<h1>¡Hola Byron Angulo!</h1>"
+    return "<h1>¡Hola Byron Andres Angulo!</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
