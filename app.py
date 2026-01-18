3c687d071d2f45c29a3ea4e683c3f722a51b90a5
Byron Angulo <baangulop1@utn.edu.ec>
2026-01-18 09:34:51 -0500

    cambio otra vez

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import flask
 app = flask(__name__)
 @app.route('/')
 def hola():
-    return "<h1>¡Hola Byron Andres Angulo!</h1>"
+    return "<h1>¡Hola Byron Andres Angulo- practica docker!</h1>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
